111ad1a1a86a67d1cf151da986d8153bf7bcdf8d
He who is brandon gulla <brandon@brandongulla.com>
2018-03-07 00:19:28 +0000

    works

--- bin/app.py
+++ bin/app.py
@@ -10,6 +10,14 @@ cors = CORS(app, resources={r"/*": {"origins": "*"}})
 def get_tasks():
     return jsonify({'sensors': ds18b20.get_readings()})
 
+@app.route('/sensor/ambient', methods=['GET'])
+def get_val():
+    return str(ds18b20.read_temp('28-00000677f123'))
+
+@app.route('/sensor/chiller', methods=['GET'])
+def get_val2():
+    return str(ds18b20.read_temp('28-00000471c98d'))
+
 if __name__ == '__main__':
     app.debug = True
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8888)
